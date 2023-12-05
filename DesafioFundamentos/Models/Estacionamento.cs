51afe66e1a44815d86b3ac2593cb655d59519a22
githubsamuell <samuel.guilherme.l@hotmail.com>
2023-12-05 20:29:46 -0300

    requisito 3

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -59,6 +59,10 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+                foreach (string item in veiculos)
+                {
+                    Console.WriteLine(item);
+                }
             }
             else
             {
